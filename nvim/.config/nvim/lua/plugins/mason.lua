e6f42d1a088cc7ebe90ee85345ba072f5a4ad573
Diegosillva <diego.silperes@gmail.com>
2025-07-06 18:13:50 -0400

    refactore

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -22,6 +22,26 @@ return {
 	},
 
 	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		lazy = false,
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"prettier", -- prettier formatter
+					"stylua", -- lua formatter
+					"isort", -- python formatter
+					"black", -- python formatter
+					"pylint",
+					"eslint_d",
+					"golangci-lint", -- Linter
+					"goimports", -- Formatter + import sorter
+					"gofumpt",
+				},
+			})
+		end,
+	},
+
+	{
 		"williamboman/mason-lspconfig.nvim",
 		lazy = false,
 		opts = {

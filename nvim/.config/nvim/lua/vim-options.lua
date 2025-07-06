fd9dc6365cc5d53b9133535d01429d9bb66c6616
Diegosillva <diego.silperes@gmail.com>
2025-07-06 17:53:24 -0400

    cursor color change
    mudança de cor nos três modos do Neovim.

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -13,6 +13,19 @@ vim.opt.signcolumn = "yes"
 vim.opt.breakindent = true
 vim.opt.smartindent = true
 
+vim.cmd([[
+  highlight CursorNormal guifg=white guibg=#5fff5f
+  highlight CursorInsert guifg=white guibg=#8C1620
+  highlight CursorVisual guifg=black guibg=#5f5fff
+]])
+
+vim.opt.guicursor = table.concat({
+	"n:block-CursorNormal", -- modo normal
+	"i:ver25-CursorInsert", -- modo insert
+	"v:block-CursorVisual", -- modo visual
+	"c:hor20-CursorCommand", -- modo command-line (opcional)
+}, ",")
+
 vim.wo.number = true
 
 vim.g.autoformat = true
